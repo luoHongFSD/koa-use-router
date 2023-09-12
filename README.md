efc8e1d3f99cfa6ff0d268589935f883655c8c04
luohong <47874742@qq.com>
2023-09-12 19:07:48 +0800

    update

--- README.md
+++ README.md
@@ -1,145 +1,50 @@
-# koa-token-bucket
+# koa-use-router
 
-Rate limiter middleware for koa.
+use router middleware for koa. this is koa-router
 
 ## Installation
 
 ```bash
 # npm
-$ npm install koa-token-bucket
+$ npm install koa-use-router
 # yarn
-$ yarn add koa-token-bucket
+$ yarn add koa-use-router
 ```
 
 ## Example
 
-### With a Redis driver
-
 ```js
-const Koa = require("koa");
-const Redis = require("ioredis");
-const app = new Koa();
-const tokenBucket = require("koa-token-bucket");
-//import tokenBucket from "koa-token-bucket";
-// apply rate limit
-app.use(
-  tokenBucket({
-    driver: "redis",
-    redis: new Redis(),
-    capacity: 100, //总令牌桶数
-    rate: 10, //1秒生成多少个令牌
-    errorMessage: "Sometimes You Just Have to Slow Down.",
-    id: (ctx) => ctx.ip,
-    headers: {
-      rate: "X-RateLimit-Rate",
-      tokens: "X-RateLimit-Tokens",
-      capacity: "X-RateLimit-Capacity",
-    },
-    disableHeader: false,
-    whitelist: (ctx) => {
-      // some logic that returns a boolean
-    },
-    blacklist: (ctx) => {
-      // some logic that returns a boolean
-    },
-  })
-);
+import Koa from "koa";
+import useRouter from "koa-use-router";
+// const userRouter = require("koa-use-router").default
+const koa = new Koa();
 
-// response middleware
-app.use(async (ctx) => {
-  ctx.body = "Stuff!";
-});
+const { createRouter, mount, group } = useRouter();
+const rootRouter = createRouter({ prefix: "/api" });
 
-// run server
-app.listen(3000, () => console.log("listening on port 3000"));
-```
+const authRouter = createRouter();
+authRouter.use(async () => {}); //中间件
+authRouter.get("/auth", async () => {}); /// /api/auth
 
-### With a Memory driver
+const v1Router = createRouter({ prefix: "/v1" });
+v1Router.use(async () => {}); // /v1 作用于v1及以下中间件
 
-```js
-const Koa = require("koa");
-const Redis = require("ioredis");
-const app = new Koa();
-const tokenBucket = require("koa-token-bucket");
-//import tokenBucket from "koa-token-bucket";
-// apply rate limit
-app.use(
-  tokenBucket({
-    driver: "memory",
-    capacity: 100, //总令牌桶数
-    rate: 10, //1秒生成多少个令牌
-    errorMessage: "Sometimes You Just Have to Slow Down.",
-    id: (ctx) => ctx.ip,
-    headers: {
-      rate: "X-RateLimit-Rate",
-      tokens: "X-RateLimit-Tokens",
-      capacity: "X-RateLimit-Capacity",
-    },
-    disableHeader: false,
-    whitelist: (ctx) => {
-      // some logic that returns a boolean
-    },
-    blacklist: (ctx) => {
-      // some logic that returns a boolean
-    },
-  })
-);
+const v2Router = createRouter({ prefix: "/v2" });
+v1Router.use(async () => {}); // /v2 作用于v2及以下中间件
 
-// response middleware
-app.use(async (ctx) => {
-  ctx.body = "Stuff!";
-});
+const userRouter = createRouter();
+userRouter.use(async () => {}); // 作用于user中间件
+userRouter.get("/user", async () => {}); // /api/v1/user or /api/v2/user
 
-// run server
-app.listen(3000, () => console.log("listening on port 3000"));
-```
+group(v1Router, [userRouter]);
+group(v2Router, [userRouter]);
+group(rootRouter, [authRouter, v1Router, v2Router]);
 
-## Options
+function routes() {
+  return mount(rootRouter);
+}
 
-- `capacity` capacity number of requests ['X-RateLimit-Capacity']
-- `rate` rate timestamp ['X-RateLimit-Rate']
-- `tokens` tokens number of requests ['X-RateLimit-Tokens']
-- `errorMessage` custom error message
-- `id` id to compare requests [ip]
-- `headers` custom header names
-- `disableHeader` set whether send the `capacity, rate, tokens` headers [false]
-- `whitelist` if function returns true, middleware exits before limiting
-- `blacklist` if function returns true, `403` error is thrown
-- `throw` call ctx.throw if true
-
-## Responses
-
-Example 200 with header fields:
-
-```
-HTTP/1.1 200 OK
-X-Powered-By: koa
-X-RateLimit-Rate: 10
-X-RateLimit-Tokens: 99
-X-RateLimit-Capacity: 100
-Content-Type: text/plain; charset=utf-8
-Content-Length: 6
-Date: Wed, 13 Nov 2013 21:22:13 GMT
-Connection: keep-alive
-
-Stuff!
-```
-
-Example 429 response:
-
-```
-HTTP/1.1 429 Too Many Requests
-X-Powered-By: koa
-X-RateLimit-Rate: 10
-X-RateLimit-Tokens: 0
-X-RateLimit-Capacity: 100
-Content-Type: text/plain; charset=utf-8
-Content-Length: 39
-Retry-After: 7
-Date: Wed, 13 Nov 2013 21:21:48 GMT
-Connection: keep-alive
-
-Rate limit exceeded
+koa.use(routes());
 ```
 
 ## License
--- index.ts
+++ index.ts
@@ -1,165 +1,37 @@
-/**
- * Module dependencies.
- */
+import KoaRouter from "koa-router";
+import compose from "koa-compose";
 
-import { Redis } from  "ioredis"
-/**
- * Expose `tokenBucket()`.
- *
- * Initialize ratelimit middleware with the given `opts`:
- *
- * - `db` database connection Redis, Map instance if memory
- * - `id` id to compare requests [ip]
- * - `headers` custom header names
- * - `tokens` tokens number of requests ['X-RateLimit-Tokens']
- * - `rate` rate timestamp ['X-RateLimit-Rate']
- * - `capacity` capacity number of requests ['X-RateLimit-Capacity']
- * - `whitelist` whitelist function [false]
- * - `blacklist` blacklist function [false]
- * - `throw` call ctx.throw if true
- *
- * @param {Object} opts
- * @return {Function}
- * @api public
- */
-export type RateLimit = {
-  driver?: "memory" | "redis";
-  redis?: Redis;
-  headers?: any;
-  id?: (ctx) => string | boolean;
-  whitelist?: (ctx) => boolean;
-  blacklist?: (ctx) => boolean;
-  disableHeader?: boolean;
-  status?: number;
-  errorMessage?: string;
-  throw?: boolean;
-  rate?: number;
-  capacity?: number;
-  namespace?: string;
-};
 
-type GetTokenOptions = {
-  tokens?:number,
-  lastRefillTime?:number,
-}
 
-module.exports = function ratelimit(options: RateLimit = {}) {
-  const defaultOpts = {
-    driver: "memory",
-    id: (ctx) => ctx.ip,
-    headers: {
-      rate: "X-RateLimit-Rate", //1秒生成多少个令牌
-      tokens: "X-RateLimit-Tokens", //当前令牌数
-      capacity: "X-RateLimit-Capacity", //总令牌桶数
-    },
-    rate: 10, //1秒生成多少个令牌
-    capacity: 100, //总令牌桶数
-    namespace: "limit",
-  };
 
-  let opts = { ...defaultOpts, ...options };
+export default function useRouter(){
+  
+   function createRouter(options={}){
+       const router = new KoaRouter(options) 
+       return router
+   }
 
-  const {
-    rate = "X-RateLimit-Rate",
-    tokens = "X-RateLimit-Tokens",
-    capacity = "X-RateLimit-Capacity",
-  } = opts.headers;
-  if(opts.driver === 'redis'&&!(opts.redis instanceof Redis)){
-    throw new Error("Invalid options reids should be ioreids instance")
-  }
-  const db = createStore(opts.driver, opts.redis, new Map());
+   function group(router,rootRoutes){
+      rootRoutes.forEach((route)=>{
+        router.use(route.routes())
+      })
+   }
 
-  function getTokens(options:GetTokenOptions = {}) {
-    const { tokens = opts.capacity, lastRefillTime = Date.now() } = options;
-    const currentTime = Date.now();
-    const timeElapsed = currentTime - lastRefillTime;
-    const tokensToAdd = (timeElapsed * opts.rate) / 1000; // 生成令牌数量
-    const currentTokens = Math.min(tokens + tokensToAdd, opts.capacity); //当前令牌数量
-
-    return {
-      tokens: currentTokens,
-      lastRefillTime: currentTime,
-    };
-  }
-
-  return async function ratelimit(ctx, next) {
-    const id = opts.id(ctx);
-    const key = `${opts.namespace}:${id}`;
-    const whitelisted =
-      typeof opts.whitelist === "function" && (await opts.whitelist(ctx));
-    const blacklisted =
-      typeof opts.blacklist === "function" && (await opts.blacklist(ctx));
-
-    if (blacklisted) {
-      ctx.throw(403, "Forbidden");
-    }
-
-    if (id === false || whitelisted) return await next();
-    let pass = false;
-    let token = getTokens(await db.get(key));
-    if (token.tokens < 1) {
-      pass = false;
-    } else {
-      token.tokens -= 1;
-      pass = true;
-    }
-
-    await db.set(key, token);
-
-    // check if header disabled
-    const disableHeader = opts.disableHeader || false;
-
-    let headers = {};
-    if (!disableHeader) {
-      // header fields
-      headers = {
-        [rate]: opts.rate,
-        [tokens]: token.tokens,
-        [capacity]: opts.capacity,
-      };
-
-      ctx.set(headers);
-    }
-
+   function mount(router){
+     return compose([router.routes(),router.allowedMethods()])
+   }
     
-    if (pass) return await next();
-
-    ctx.status = opts.status || 429;
-    ctx.body = opts.errorMessage || `Rate limit exceeded.`;
-
-    if (opts.throw) {
-      ctx.throw(ctx.status, ctx.body, { headers });
-    }
-  };
-};
-
-function createStore(driver, redis, map) {
-  let db;
-  if (driver === "redis") {
-    db = {
-      async get(key) {
-        const value = await redis.get(key)
-        if(value){
-          return JSON.parse(value)
-        }else{
-          return undefined
-        }
-      },
-      async set(key, value) {
-        return redis.set(key, JSON.stringify(value));
-      },
-    };
-  } else {
-    db = {
-      async get(key) {
-        const value = map.get(key);
-        return await value;
-      },
-      async set(key, value) {
-        map.set(key, value);
-        return await value;
-      },
-    };
-  }
-  return db;
+   return {
+    createRouter,
+    group,
+    mount
+   }
 }
+
+
+
+
+
+
+
+ 
